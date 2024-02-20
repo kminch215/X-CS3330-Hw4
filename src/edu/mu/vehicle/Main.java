ca44a2b1d61f9cb2000a6f813215dc8600ae758a
Samantha Whitaker <s.whita6@gmail.com>
2024-02-20 11:46:01 -0600

    Updated Main to call new VehicleManager constructor

--- src/edu/mu/vehicle/Main.java
+++ src/edu/mu/vehicle/Main.java
@@ -5,10 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		VehicleManager.getInstance();
 	    
-	    boolean error = VehicleManager.getInstance().readFromFile("vehicleList.csv");
-	    if(!error) {
-	    	System.out.println("readFromFile unsuccessful");
-	    }
+	    VehicleManager manager = new VehicleManager("vehicleList.csv");
 
 	}
 
