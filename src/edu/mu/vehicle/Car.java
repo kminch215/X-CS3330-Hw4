3bb023d57fc71b44f952e001ca6a1469470753f3
kminch215 <kendraminch1036@gmail.com>
2024-02-19 21:08:14 -0600

    Creating Car Class
    Creating the file for the Car Class that extends the Vehicle Class. This
    class also implements the abstract methods along with the parameterized
    constructor and the copy constructor.

---
+++ src/edu/mu/vehicle/Car.java
@@ -0,0 +1,41 @@
+package edu.mu.vehicle;
+
+import java.time.Year;
+
+public class Car extends Vehicle {
+
+	//parameterized constructor
+	public Car(String brand, String make, long modelYear, double price, VehicleColor color, FuelType fuelType,
+			double mileage, double mass, int cylinders, double gasTankCapacity, StartMechanism startType) {
+		super(brand, make, modelYear, price, color, fuelType, mileage, mass, cylinders, gasTankCapacity, startType);
+		// TODO Auto-generated constructor stub
+	}
+
+	//copy constructor
+	public Car(Vehicle vehicle) {
+		super(vehicle);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double calculateMaintenaceCost(double distance) {
+		double maintenanceCost = distance * mass * (Year.now().getValue() - modelYear) * cylinders * 0.0005;
+		
+		return maintenanceCost;
+	}
+
+	@Override
+	public double calculateFuelEfficiency(double distance, double fuelPrice) {
+		double fuelEfficiency = cylinders * gasTankCapacity * fuelPrice / distance * 0.003;
+		
+		return fuelEfficiency;
+	}
+
+	@Override
+	public void startEngine() {
+		startType = StartMechanism.PUSHSTART;
+	}
+
+	
+	
+}
