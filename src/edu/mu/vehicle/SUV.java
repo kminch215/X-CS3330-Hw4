27f30822930803391b85cf06cceed4aba56e1fe4
kminch215 <kendraminch1036@gmail.com>
2024-02-19 21:28:59 -0600

    Taking out unnecessary comments

--- src/edu/mu/vehicle/SUV.java
+++ src/edu/mu/vehicle/SUV.java
@@ -8,13 +8,11 @@ public class SUV extends Vehicle {
 	public SUV(String brand, String make, long modelYear, double price, VehicleColor color, FuelType fuelType,
 			double mileage, double mass, int cylinders, double gasTankCapacity, StartMechanism startType) {
 		super(brand, make, modelYear, price, color, fuelType, mileage, mass, cylinders, gasTankCapacity, startType);
-		// TODO Auto-generated constructor stub
 	}
 
 	//copy constructor
 	public SUV(Vehicle vehicle) {
 		super(vehicle);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
--- src/edu/mu/vehicle/Truck.java
+++ src/edu/mu/vehicle/Truck.java
@@ -8,13 +8,11 @@ public class Truck extends Vehicle {
 	public Truck(String brand, String make, long modelYear, double price, VehicleColor color, FuelType fuelType,
 			double mileage, double mass, int cylinders, double gasTankCapacity, StartMechanism startType) {
 		super(brand, make, modelYear, price, color, fuelType, mileage, mass, cylinders, gasTankCapacity, startType);
-		// TODO Auto-generated constructor stub
 	}
 
 	//copy constructor
 	public Truck(Vehicle vehicle) {
 		super(vehicle);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
@@ -35,5 +33,6 @@ public class Truck extends Vehicle {
 	public void startEngine() {
 		this.startType = StartMechanism.KEYSTART;
 	}
+	
 
 }
