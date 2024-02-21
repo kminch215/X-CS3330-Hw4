9f3f2e29027e1fe78e8b910d8a5c357abd22a808
Samantha Whitaker <s.whita6@gmail.com>
2024-02-21 17:07:49 -0600

    Added additional tests in Main for fuelEfficiency methods

--- src/edu/mu/vehicle/Main.java
+++ src/edu/mu/vehicle/Main.java
@@ -1,15 +1,31 @@
 package edu.mu.vehicle;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
 		VehicleManager.getInstance();
 	    
+		// Read vehicle data from the vehicleList.csv file and initialize objects.
 		boolean error = VehicleManager.getInstance().initializeStock();
 		if(!error) {
 			System.out.println("Error initializing vehicle stock from file");
 		}
-
+		
+		// Determines vehicle with highest fuel efficiency
+		ArrayList<Vehicle> bestFuelEfficiency = VehicleManager.getInstance().getVehicleWithHighestFuelEfficiency(300, 3.25);
+		System.out.println("Vehicle(s) with the highest fuel efficiency: ");
+		for(Vehicle vehicle : bestFuelEfficiency) {
+			System.out.println(vehicle.toString());
+		}
+		
+//		 Determines vehicle with lowest fuel efficiency
+		ArrayList<Vehicle> worstFuelEfficiency = VehicleManager.getInstance().getVehicleWithLowestFuelEfficiency(300, 3.25);
+		System.out.println("Vehicle(s) with the lowest fuel efficiency: ");
+		for(Vehicle vehicle : worstFuelEfficiency) {
+			System.out.println(vehicle.toString());
+		}
 	}
 
 }
