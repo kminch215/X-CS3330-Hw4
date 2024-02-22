6c01667a932396e426c3d08923060bd9b079a8a1
sarahkgreenwood <149001674+sarahkgreenwood@users.noreply.github.com>
2024-02-22 14:27:14 -0600

    Update Car.java
    'Maintenance' was spelled wrong

--- src/edu/mu/vehicle/Car.java
+++ src/edu/mu/vehicle/Car.java
@@ -16,7 +16,7 @@ public class Car extends Vehicle {
 	}
 
 	@Override
-	public double calculateMaintenaceCost(double distance) {
+	public double calculateMaintenanceCost(double distance) {
 		double maintenanceCost = distance * mass * (Year.now().getValue() - modelYear) * cylinders * 0.0005;
 		
 		return maintenanceCost;
